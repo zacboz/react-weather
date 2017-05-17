f5d368d53d0550c2277de6eeffc92681a1dc359b
zacboz <zacboz@gmail.com>
2017-05-17 16:32:30 -0600

    framework of react app, main and nav components

---
+++ app/app.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router'); // = require('react-router').Route
+var Main = require('Main');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
