fe169476fc39db4286cf0d1cccf61e5016239caa
zacboz <zacboz@gmail.com>
2017-05-17 22:08:00 -0600

    axios and connected api to app

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,22 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=ba4d1cf7db25286d3784227e1e3cc161&units=imperial';
+// ba4d1cf7db25286d3784227e1e3cc161
+
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function(res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function(res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
