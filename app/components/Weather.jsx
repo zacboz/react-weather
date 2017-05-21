6ff3b8ad02f8eed8a026dfef446e14f6d06d2d76
zacboz <zacboz@gmail.com>
2017-05-21 00:05:09 -0600

    fixed error modal bug

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -15,7 +15,9 @@ var Weather = React.createClass({
 
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
 
     openWeatherMap.getTemp(location).then(function(temp) {
@@ -32,7 +34,22 @@ var Weather = React.createClass({
       alert(errorMessage);
     });
   },
+  componentDidMount: function () {
+    var location = this.props.location.query.location;
 
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function(newProps) {
+    var location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
   render: function () {
     var {isLoading, location, temp, errorMessage} = this.state;
 
